c8dfe81118274042dc040bd744a14756054c0b6f
Adriano <adrianosenai604@gmail.com>
2025-11-04 15:46:35 -0300

    fix(cors): permitir requisições do front na Vercel

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors({
+  origin: "https://projeto-simples-front.vercel.app"
+}))
+
 app.get("/", (req, res) => {
   res.json({ message: "API funcionando com CI/CD no Render..." })
 })
