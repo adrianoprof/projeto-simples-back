34529d0c417fd77887d0f526e553431cea17ce9c
Adriano <adrianosenai604@gmail.com>
2026-04-09 20:16:02 -0300

    fix(server): atualizar mensagem do get inicial

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
-  res.json({ message: "API funcionando com CI/CD no Render..." })
+  res.json({ message: "API funcionando com CI/CD no Render 2..." })
 })
 
 app.listen(PORT, () => {
